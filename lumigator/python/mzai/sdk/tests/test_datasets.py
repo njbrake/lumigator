0388393f6d767f5b1d4a5ad75faf9df813205f5a
Javier Torres <javier@mozilla.ai>
2024-10-08 13:34:38 +0200

    Add integration tests

--- lumigator/python/mzai/sdk/tests/test_datasets.py
+++ lumigator/python/mzai/sdk/tests/test_datasets.py
@@ -11,7 +11,6 @@ from tests.helpers import check_method, load_json
 def test_get_datasets_ok(mock_requests_response, mock_requests, lumi_client, json_data_datasets):
     mock_requests_response.status_code = 200
     data = load_json(json_data_datasets)
-    print(f"test data is: {data}")
     mock_requests_response.json = lambda: data
 
     datasets = lumi_client.datasets.get_datasets()
@@ -39,6 +38,17 @@ def test_get_dataset_ok(mock_requests_response, mock_requests, lumi_client, json
 
 def test_delete_dataset_ok(mock_requests_response, mock_requests, lumi_client, json_data_dataset):
     mock_requests_response.status_code = 204
+    data = load_json(json_data_dataset)
+    mock_requests_response.json = lambda: None
+    mock_requests.side_effect = lambda **kwargs: check_method(str(HTTPMethod.DELETE), **kwargs)
+
+    lumi_client.datasets.delete_dataset(UUID(data["id"]))
+
+
+def test_delete_dataset_not_found(
+    mock_requests_response, mock_requests, lumi_client, json_data_dataset
+):
+    mock_requests_response.status_code = 404
     data = load_json(json_data_dataset)
     mock_requests_response.json = lambda: None
     mock_requests.side_effect = lambda **kwargs: check_method(str(HTTPMethod.DELETE), **kwargs)
