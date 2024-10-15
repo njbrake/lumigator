0884ea0419c581eed350be759aefcc7dfc7ef2e3
Javier Torres <javier@mozilla.ai>
2024-10-15 11:47:36 +0200

    Format updated

--- lumigator/python/mzai/sdk/tests/int_test_datasets.py
+++ lumigator/python/mzai/sdk/tests/int_test_datasets.py
@@ -5,6 +5,7 @@ from loguru import logger
 
 from tests.helpers import load_json, check_method
 
+
 def test_get_datasets_remote_ok(lumi_client):
     datasets = lumi_client.datasets.get_datasets()
     assert datasets is not None
@@ -12,9 +13,17 @@ def test_get_datasets_remote_ok(lumi_client):
 
 def test_dataset_lifecycle_remote_ok(lumi_client, hf_data):
     with Path.open(hf_data) as file:
-        dataset = lumi_client.datasets.create_dataset(
-            dataset=file, format=DatasetFormat.EXPERIMENT
-        )
+        dataset = lumi_client.datasets.create_dataset(dataset=file, format=DatasetFormat.EXPERIMENT)
+        datasets = lumi_client.datasets.get_datasets()
+        assert datasets.total is not 0
+        dataset = lumi_client.datasets.get_dataset(datasets.items[0].id)
+        assert dataset is not None
+        lumi_client.datasets.delete_dataset(datasets.items[0].id)
+
+
+def test_deployment_lifecycle_remote_ok(lumi_client, hf_data):
+    with Path.open(hf_data) as file:
+        dataset = lumi_client.datasets.create_dataset(dataset=file, format=DatasetFormat.EXPERIMENT)
         datasets = lumi_client.datasets.get_datasets()
         assert datasets.total is not 0
         dataset = lumi_client.datasets.get_dataset(datasets.items[0].id)
