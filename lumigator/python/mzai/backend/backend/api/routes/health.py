34d8c245332ff0790a39b53e2630de588066c309
Davide Eynard <davide@mozilla.ai>
2024-10-30 09:12:50 +0000

    Adding job logs support to health endpoint

--- lumigator/python/mzai/backend/backend/api/routes/health.py
+++ lumigator/python/mzai/backend/backend/api/routes/health.py
@@ -4,7 +4,7 @@ from uuid import UUID
 import requests
 from fastapi import APIRouter
 from schemas.extras import HealthResponse
-from schemas.jobs import JobSubmissionResponse
+from schemas.jobs import JobLogsResponse, JobSubmissionResponse
 
 from backend.settings import settings
 
@@ -23,6 +23,21 @@ def get_job_metadata(job_id: UUID) -> JobSubmissionResponse:
         try:
             metadata = json.loads(resp.text)
             return JobSubmissionResponse(**metadata)
+        except json.JSONDecodeError as e:
+            print(f"JSON decode error: {e}")
+            print(f"Response text: {resp.text}")
+            return {"error": "Invalid JSON response"}
+    else:
+        return {"error": f"HTTP error {resp.status_code}"}
+
+
+@router.get("/jobs/{job_id}/logs")
+def get_job_logs(job_id: UUID) -> JobLogsResponse:
+    resp = requests.get(f"{settings.RAY_DASHBOARD_URL}/api/jobs/{job_id}/logs")
+    if resp.status_code == 200:
+        try:
+            metadata = json.loads(resp.text)
+            return JobLogsResponse(**metadata)
         except json.JSONDecodeError as e:
             print(f"JSON decode error: {e}")
             print(f"Response text: {resp.text}")
